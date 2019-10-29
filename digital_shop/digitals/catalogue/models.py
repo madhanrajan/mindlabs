318fcda0d5a72c900801e4e03b27495a3841ffad
Madhan Rajan <madhanrajan.varadharajan@gmail.com>
2019-10-29 18:16:48 +0000

    update

--- digital_shop/digitals/catalogue/models.py
+++ digital_shop/digitals/catalogue/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from oscar.apps.catalogue.abstract_models import AbstractProduct
 
 
+
 class Product(AbstractProduct):
     file = models.FileField(upload_to='files/%Y/%m/%d')
 
+    def get_product_url(self):
+        return self.file.url
 
 from oscar.apps.catalogue.models import *  # noqa isort:skip
